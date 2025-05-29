aa473b0219e5b8bd65c71caba54b9dfcf45d4eda
Paulo Braga <paulocbraga@yahoo.com.br>
2025-05-29 12:17:00 -0300

    fix: ajusta caminhos dos assets para relativos

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
